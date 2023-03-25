ec3d569c83a4a9c2474c9e3983a018a842459722
Rodrigo <rodriguesrs1@gmail.com>
2023-03-24 22:35:49 -0300

    Sinc Pasta

--- src/TiposDeVariaveis/Tipos.java
+++ src/TiposDeVariaveis/Tipos.java
@@ -1,10 +1,17 @@
-package TiposDeVariaveis;
+	package TiposDeVariaveis;
 
 public class Tipos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int numero1 = 1;
+		int numero2 = 2;
+		
+		boolean simNao = numero1 == numero2;
+		System.out.println("numeroUm é igual a numeroDois? " + simNao);
 
+		
+		
 	}
 
 }
---
+++ src/metodosDio/metodosTreinamento.java
@@ -0,0 +1,10 @@
+package metodosDio;
+
+public class metodosTreinamento {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
